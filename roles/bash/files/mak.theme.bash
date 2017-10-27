b9afdb0fb9a765f2973c3bc382c3e5bfb6f2cad7
Matthias Krull <m.krull@uninets.eu>
2017-10-27 11:09:41 +0200

    feat(bash): added convenience function to switch kubectl configs

--- roles/bash/files/mak.theme.bash
+++ roles/bash/files/mak.theme.bash
@@ -7,8 +7,17 @@ SCM_THEME_PROMPT_CLEAN=" ${green}✓"
 VIRTUALENV_THEME_PROMPT_PREFIX="<"
 VIRTUALENV_THEME_PROMPT_SUFFIX=">"
 
+KUBEON_THEME_PROMPT_PREFIX="["
+KUBEON_THEME_PROMPT_SUFFIX="]"
+
+kubeon-promt() {
+    if [ "${KUBEON_PROMPT}" ]; then
+        echo -n ${KUBEON_THEME_PROMPT_PREFIX}${KUBEON_PROMPT}${KUBEON_THEME_PROMPT_SUFFIX}
+    fi
+}
+
 prompt() {
-    PS1="${cyan}\W${reset_color} $(scm_prompt_info)${reset_color} ${cyan}$(virtualenv_prompt)${reset_color}
+    PS1="${cyan}\W${reset_color} $(scm_prompt_info)${reset_color} ${cyan}$(virtualenv_prompt)${reset_color} ${blue}$(kubeon-promt)${reset_color}
 ${blue}>${reset_color} "
 }
 
