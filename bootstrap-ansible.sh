e9647db224087cba13d7f610ed2b530e5d7ad197
Matthias Krull <m.krull@uninets.eu>
2017-10-29 13:17:01 +0100

    wip. support bootstrapping arch linux as well

--- bootstrap-ansible.sh
+++ bootstrap-ansible.sh
@@ -1,7 +1,25 @@
 #!/bin/bash
 
-sudo apt-get update
-sudo apt-get install software-properties-common
-sudo apt-add-repository ppa:ansible/ansible
-sudo apt-get update
-sudo apt install ansible
+function install-ubuntu {
+    sudo apt-get update
+    sudo apt-get install software-properties-common
+    sudo apt-add-repository ppa:ansible/ansible
+    sudo apt-get update
+    sudo apt install ansible
+}
+
+function install-arch {
+    sudo pacman -S ansible
+}
+
+# get distribution
+eval $(cat /etc/*release|grep ^ID)
+
+if [ "${ID}" ]; then
+    case ${ID} in
+    arch|antergos) install-arch ;;
+    debian|ubuntu) install-ubuntu ;;
+    *) echo "Unknown distribution" ;;
+    esac
+fi
+
